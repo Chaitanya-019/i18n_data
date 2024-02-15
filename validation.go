4af58d4258e679fe8d895ff40584f8f2ddfa2f03
Chaitanya-019 <betha.chaitanya19@gmail.com>
2024-02-15 16:35:42 +0530

    Added validations

--- validation.go
+++ validation.go
@@ -8,9 +8,9 @@ import (
 	"github.com/xeipuuv/gojsonschema"
 )
 
-func main() {
+func validate(scheme_file, data_file string) {
 	// Read the JSON schema file
-	schemaFile, err := ioutil.ReadFile("country_tele_scheme.json")
+	schemaFile, err := ioutil.ReadFile(scheme_file)
 	if err != nil {
 		fmt.Println("Error reading schema file:", err)
 		os.Exit(1)
@@ -20,7 +20,7 @@ func main() {
 	schemaLoader := gojsonschema.NewStringLoader(string(schemaFile))
 
 	// Read the JSON data file
-	dataFile, err := ioutil.ReadFile("country_tele_data.json")
+	dataFile, err := ioutil.ReadFile(data_file)
 	if err != nil {
 		fmt.Println("Error reading data file:", err)
 		os.Exit(1)
@@ -33,7 +33,7 @@ func main() {
 	result, err := gojsonschema.Validate(schemaLoader, dataLoader)
 	if err != nil {
 		fmt.Println("Error:", err)
-		os.Exit(1)
+		panic(err)
 	}
 
 	// Check validation result
@@ -45,5 +45,24 @@ func main() {
 		for _, desc := range result.Errors() {
 			fmt.Printf("- %s\n", desc)
 		}
+		panic("Cross check the data with Scheme!")
 	}
+}
+
+func main() {
+	fmt.Println("Validating Country Information!")
+	validate("country_info_scheme.json", "country_info_data.json")
+
+	fmt.Println("\n Validating Country Language Information!")
+	validate("country_language_scheme.json", "country_language_data.json")
+
+	fmt.Println("\n Validating Country Currency Information!")
+	validate("country_currency_scheme.json", "country_currency_data.json")
+
+	fmt.Println("\n Validating Country tele Information!")
+	validate("country_tele_scheme.json", "country_tele_data.json")
+
+	fmt.Println("\n Validating Country Timezones Information!")
+	validate("country_timezones_scheme.json", "country_timezones_data.json")
+
 }
